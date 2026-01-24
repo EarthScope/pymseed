2326a8a45b3563c116b83b043b691e1a63e0eeb0
chad-earthscope <chad.trabant@earthscope.org>
2026-01-24 15:30:16 -0800

    Update libmseed to v3.2.4

--- cffi_module/libmseed/libmseed.h
+++ cffi_module/libmseed/libmseed.h
@@ -29,8 +29,8 @@ extern "C"
 {
 #endif
 
-#define LIBMSEED_VERSION "3.2.3"    //!< Library version
-#define LIBMSEED_RELEASE "2026.001" //!< Library release date
+#define LIBMSEED_VERSION "3.2.4"    //!< Library version
+#define LIBMSEED_RELEASE "2026.024" //!< Library release date
 
 /** @defgroup io-functions File and URL I/O */
 /** @defgroup miniseed-record Record Handling */
@@ -1277,6 +1277,7 @@ extern MSLogParam *ms_rloginit_l (MSLogParam *logp, void (*log_print) (const cha
                                   const char *logprefix, void (*diag_print) (const char *),
                                   const char *errprefix, int maxmessages);
 extern int ms_rlog_emit (MSLogParam *logp, int count, int context);
+extern int ms_rlog_pop (MSLogParam *logp, char *message, size_t size, int context);
 extern int ms_rlog_free (MSLogParam *logp);
 
 /** @} */
