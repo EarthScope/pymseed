b3254eea1623451720ef99ca64983596e717ca9e
chad-earthscope <chad.trabant@earthscope.org>
2026-05-17 23:20:22 -0700

    cleanup definitions.py

--- src/pymseed/definitions.py
+++ src/pymseed/definitions.py
@@ -7,13 +7,7 @@ This module provides enums for some of the grouped constants.
 
 from enum import IntEnum
 
-# Import the CFFI interface
-try:
-    from .clib import clibmseed
-except ImportError:
-    raise ImportError(
-        "CFFI interface not available. Please build the CFFI interface first."
-    ) from None
+from .clib import clibmseed
 
 
 class DataEncoding(IntEnum):
@@ -27,48 +21,52 @@ class DataEncoding(IntEnum):
     STEIM1 = clibmseed.DE_STEIM1  # Steim-1 compressed 32-bit integers
     STEIM2 = clibmseed.DE_STEIM2  # Steim-2 compressed 32-bit integers
 
+    # Legacy encodings supported by libmseed for decoding only
+    GEOSCOPE24 = clibmseed.DE_GEOSCOPE24
+    GEOSCOPE163 = clibmseed.DE_GEOSCOPE163
+    GEOSCOPE164 = clibmseed.DE_GEOSCOPE164
+    CDSN = clibmseed.DE_CDSN
+    SRO = clibmseed.DE_SRO
+    DWWSSN = clibmseed.DE_DWWSSN
+
 
 class TimeFormat(IntEnum):
     """Time format codes for ms_nstime2timestr() and ms_nstime2timestrz()"""
 
-    ISOMONTHDAY = (
-        clibmseed.ISOMONTHDAY
-    )  # "YYYY-MM-DDThh:mm:ss.sssssssss", ISO 8601 in month-day format
-    ISOMONTHDAY_Z = (
-        clibmseed.ISOMONTHDAY_Z
-    )  # "YYYY-MM-DDThh:mm:ss.sssssssss", ISO 8601 in month-day format with trailing Z
-    ISOMONTHDAY_DOY = (
-        clibmseed.ISOMONTHDAY_DOY
-    )  # "YYYY-MM-DD hh:mm:ss.sssssssss (doy)", ISOMONTHDAY with day-of-year
-    ISOMONTHDAY_DOY_Z = (
-        clibmseed.ISOMONTHDAY_DOY_Z
-    )  # "YYYY-MM-DD hh:mm:ss.sssssssss (doy)", ISOMONTHDAY with day-of-year and trailing Z
-    ISOMONTHDAY_SPACE = (
-        clibmseed.ISOMONTHDAY_SPACE
-    )  # "YYYY-MM-DD hh:mm:ss.sssssssss", same as ISOMONTHDAY with space separator
-    ISOMONTHDAY_SPACE_Z = (
-        clibmseed.ISOMONTHDAY_SPACE_Z
-    )  # "YYYY-MM-DD hh:mm:ss.sssssssss", same as ISOMONTHDAY with space separator and trailing Z
-    SEEDORDINAL = clibmseed.SEEDORDINAL  # "YYYY,DDD,hh:mm:ss.sssssssss", SEED day-of-year format
-    UNIXEPOCH = clibmseed.UNIXEPOCH  # "ssssssssss.sssssssss", Unix epoch value
-    NANOSECONDEPOCH = clibmseed.NANOSECONDEPOCH  # "sssssssssssssssssss", Nanosecond epoch value
+    #: ``YYYY-MM-DDThh:mm:ss.sssssssss``, ISO 8601 in month-day format
+    ISOMONTHDAY = clibmseed.ISOMONTHDAY
+    #: ``YYYY-MM-DDThh:mm:ss.sssssssss``, ISO 8601 in month-day format with trailing ``Z``
+    ISOMONTHDAY_Z = clibmseed.ISOMONTHDAY_Z
+    #: ``YYYY-MM-DD hh:mm:ss.sssssssss (doy)``, ISOMONTHDAY with day-of-year
+    ISOMONTHDAY_DOY = clibmseed.ISOMONTHDAY_DOY
+    #: ``YYYY-MM-DD hh:mm:ss.sssssssss (doy)``, ISOMONTHDAY with day-of-year and trailing ``Z``
+    ISOMONTHDAY_DOY_Z = clibmseed.ISOMONTHDAY_DOY_Z
+    #: ``YYYY-MM-DD hh:mm:ss.sssssssss``, same as ISOMONTHDAY with space separator
+    ISOMONTHDAY_SPACE = clibmseed.ISOMONTHDAY_SPACE
+    #: ``YYYY-MM-DD hh:mm:ss.sssssssss``, same as ISOMONTHDAY with space separator and trailing ``Z``
+    ISOMONTHDAY_SPACE_Z = clibmseed.ISOMONTHDAY_SPACE_Z
+    #: ``YYYY,DDD,hh:mm:ss.sssssssss``, SEED day-of-year format
+    SEEDORDINAL = clibmseed.SEEDORDINAL
+    #: ``ssssssssss.sssssssss``, Unix epoch value
+    UNIXEPOCH = clibmseed.UNIXEPOCH
+    #: ``sssssssssssssssssss``, Nanosecond epoch value
+    NANOSECONDEPOCH = clibmseed.NANOSECONDEPOCH
 
 
 class SubSecond(IntEnum):
     """Subsecond resolution codes for ms_nstime2timestr() and ms_nstime2timestrz()"""
 
-    NONE = clibmseed.NONE  # No subseconds
-    MICRO = clibmseed.MICRO  # Microsecond resolution
-    NANO = clibmseed.NANO  # Nanosecond resolution
-    MICRO_NONE = (
-        clibmseed.MICRO_NONE
-    )  # Microsecond resolution if subseconds are non-zero, otherwise no subseconds
-    NANO_NONE = (
-        clibmseed.NANO_NONE
-    )  # Nanosecond resolution if subseconds are non-zero, otherwise no subseconds
-    NANO_MICRO = (
-        clibmseed.NANO_MICRO
-    )  # Nanosecond resolution if there are sub-microseconds, otherwise microseconds resolution
-    NANO_MICRO_NONE = (
-        clibmseed.NANO_MICRO_NONE
-    )  # Nanosecond resolution if present, microsecond if present, otherwise no subseconds
+    #: No subseconds
+    NONE = clibmseed.NONE
+    #: Microsecond resolution
+    MICRO = clibmseed.MICRO
+    #: Nanosecond resolution
+    NANO = clibmseed.NANO
+    #: Microsecond resolution if subseconds are non-zero, otherwise no subseconds
+    MICRO_NONE = clibmseed.MICRO_NONE
+    #: Nanosecond resolution if subseconds are non-zero, otherwise no subseconds
+    NANO_NONE = clibmseed.NANO_NONE
+    #: Nanosecond resolution if there are sub-microseconds, otherwise microseconds resolution
+    NANO_MICRO = clibmseed.NANO_MICRO
+    #: Nanosecond resolution if present, microsecond if present, otherwise no subseconds
+    NANO_MICRO_NONE = clibmseed.NANO_MICRO_NONE
